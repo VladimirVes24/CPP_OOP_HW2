73f8884b91132b4383d31eaff9eedf3d3be30a3f
VLADIMIR <veselov2100@mail.ru>
2021-08-30 17:34:41 +0300

    Complete

--- CPP_OOP_HW2.cpp
+++ CPP_OOP_HW2.cpp
@@ -1,20 +1,167 @@
-// CPP_OOP_HW2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+//Task 1 ================================================================================================================================
+
+class Person
+{
+public:
+    
+    void setGender(std::string inputGender)
+    {
+        gender = inputGender;
+    }
+
+    void setName(std::string inputName)
+    {
+        name = inputName;
+    }
+
+    void setWeight(int inputWeight)
+    {
+        weight = inputWeight;
+    }
+
+    void setAge(int inputAge)
+    {
+        age = inputAge;
+    }
+
+    std::string getName()
+    {
+        return name;
+    }
+
+    std::string getGender()
+    {
+        return gender;
+    }
+
+    int getAge()
+    {
+        return age;
+    }
+
+    int getWeight()
+    {
+        return weight;
+    }
+
+private:
+    std::string name, gender;
+    int age = 0, weight = 0;
+};
+
+class Student : public Person
+{
+public:
+    Student(std::string inputGender, std::string inputName, int inputWeight, int inputAge, int inputYear, int *counter)
+    {
+        this->setAge(inputAge);
+        this->setGender(inputGender);
+        this->setName(inputName);
+        this->setWeight(inputWeight);
+        this->setYear(inputYear);
+        *counter = *counter + 1;
+
+    }
+    void setYear(int inputYear)
+    {
+        studyYear = inputYear;
+    }
+
+    void addYear()
+    {
+        studyYear++;
+    }
+
+private:
+    int studyYear;
+};
+
+//Task 2 =====================================================================================================================
+
+class Fruit
+{
+public:
+    std::string getName()
+    {
+        return name;
+    }
+
+    std::string getColor()
+    {
+        return color;
+    }
+
+    void setName(std::string input)
+    {
+        name = input;
+    }
+
+    void setColor(std::string input)
+    {
+        color = input;
+    }
+private:
+    std::string name, color;
+};
+
+class Apple : public Fruit
+{
+public:
+    Apple(){}
+    Apple(std::string input)
+    {
+        this->setName("apple");
+        this->setColor(input);
+    }
+};
+
+class Banana : public Fruit
+{
+public:
+    Banana()
+    {
+        this->setName("banana");
+        this->setColor("yellow");
+    }
+};
+
+class GrannySmith : public Apple
+{
+public:
+    GrannySmith()
+    {
+        this->setName("Granny Smith apple");
+        this->setColor("green");
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //Task 1 =========================================================================================================================
+    int counter = 0;
+    Student* firstStudent = new Student("Male", "John", 83, 18, 1, &counter);
+    Student* secondStudent = new Student("Alex", "Female", 53, 18, 1, &counter);
+    Student* thirdStudent = new Student("Mary", "Female", 68, 20, 3, &counter);
+    std::cout << "Student 1: " << firstStudent->getName() << "\n" << "Total number of students: " << counter << "\n\n";
+
+    delete firstStudent;
+    delete secondStudent;
+    delete thirdStudent;
+    counter = 0;
+
+    //Task 2 =========================================================================================================================
+    
+    Apple a("red");
+    Banana b;
+    GrannySmith c;
+
+    std::cout << "My " << a.getName() << " is " << a.getColor() << ".\n";
+    std::cout << "My " << b.getName() << " is " << b.getColor() << ".\n";
+    std::cout << "My " << c.getName() << " is " << c.getColor() << ".\n";
+
+    return 0;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
